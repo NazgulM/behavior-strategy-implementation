fa7511062e36cbf7f100e51a2cc0cbd1a727393d
NazgulM <maniyasova.n@gmail.com>
2022-07-01 22:34:51 +0200

    dot repeat

--- 1-remix/parsons-problems/repeat-string/use-built-in-methods/dot-repeat.js
+++ 1-remix/parsons-problems/repeat-string/use-built-in-methods/dot-repeat.js
@@ -13,8 +13,27 @@ const repeatString = (text = '', repetitions = 1) => {
   return text.repeat(repetitions);
 };
 
-return repetitions.repeat(text); // distractor
-return repeat(text, repetitions); // distractor
-return repeat(repetitions, text); // distractor
+// return repetitions.repeat(text); // distractor
+// return repeat(text, repetitions); // distractor
+// return repeat(repetitions, text); // distractor
 
-const repeatString = (text: '', repetitions: 1) => { // distractor
+// const repeatString = (text: '', repetitions: 1) => { // distractor
+
+
+describe("Tests", () => {
+  it("test if the string can repeat ", () => {
+    expect(repeatString('', 0)).toEqual('');
+  });
+});
+
+describe("Tests", () => {
+  it("test if the string can repeat n times", () => {
+    expect(repeatString('aruuke', 1)).toEqual('aruuke');
+  });
+});
+
+describe("Tests", () => {
+  it("test if the string can repeat n times", () => {
+    expect(repeatString('aruuke', 2)).toEqual('aruukearuuke');
+  });
+});
